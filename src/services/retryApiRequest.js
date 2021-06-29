609f358ba908b0d214fdb247d755d6270dc50c2d
Tuukka Virtanen <duukkis@gmail.com>
2021-06-30 02:24:08 +0300

    improved axios response interceptor logging

--- src/services/retryApiRequest.js
+++ src/services/retryApiRequest.js
@@ -1,14 +1,19 @@
 const axiosResponseInterceptor = ( axiosInstance, baseUrl ) => {
 	// axios response interceptor. Request is sent again if the array of server response is empty.
 	axiosInstance.interceptors.response.use( response => {
-		console.log( 'axios response interceptor:' )
-		console.log( 'response in server response:', response )
-		console.log( 'response.data.length in server response:', response.data.length )
+		if( response.data.length > 2 ) {
+			console.log( 
+				'valid server response detected with parameter:', response.config.params
+			)
+
+			return response
+		}
 
 		if( response.config.params.manufacturer ) {
 			if( response.data.length <= 2 ) {
 				console.error( 
-					'invalid server response detected with parameter:', response.config.params.manufacturer,
+					'invalid server response detected with product manufacturer parameter:', 
+					response.config.params.manufacturer,
 					'response.data.length:', response.data.length,
 					'requesting data again...' 
 				)
@@ -16,8 +21,6 @@ const axiosResponseInterceptor = ( axiosInstance, baseUrl ) => {
 				return axiosInstance.get( baseUrl, { params: { manufacturer: response.config.params.manufacturer } } )
 			}
 		}
-
-		return response
 	}, error => {
 		return Promise.reject( 'error occurred during API request:', error )
 	} )
